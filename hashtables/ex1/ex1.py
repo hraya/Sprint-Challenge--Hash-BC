65a168387f6921d274783af641f4a4ee7af28a0e
hraya <hraya@lssu.edu>
2020-04-19 11:38:46 -0500

    hashtables ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,9 +9,13 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(len(weights)):
+        bal = limit - weights[i]
+        bal_index = hash_table_retrieve(ht,bal)
+        if bal_index is None:
+            hash_table_insert(ht,weights[i], i)
+        else:
+            return (i, bal_index)
 
     return None
 
